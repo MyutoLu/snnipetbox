9feac1f910e46403981f63dcedca9299163be246
myuto <1014145596@qq.com>
2024-05-03 14:07:53 +0800

    13.1

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"html/template"
+	"io/fs"
 	"myuto.net/snippetbox/internal/models"
+	"myuto.net/snippetbox/ui"
 	"path/filepath"
 	"time"
 )
@@ -28,28 +30,35 @@ var functions = template.FuncMap{
 func newTemplateCache() (map[string]*template.Template, error) {
 	cache := map[string]*template.Template{}
 
-	pages, err := filepath.Glob("./ui/html/pages/*.tmpl")
+	pages, err := fs.Glob(ui.Files, "html/pages/*.tmpl")
 	if err != nil {
 		return nil, err
 	}
 
 	for _, page := range pages {
 		name := filepath.Base(page)
+
+		patterns := []string{
+			"html/base.tmpl",
+			"html/partials/*.tmpl",
+			page,
+		}
+
 		// Create a new template set by parsing the 'base.tmpl' template.
-		ts, err := template.New(name).Funcs(functions).ParseFiles("./ui/html/base.tmpl")
+		ts, err := template.New(name).Funcs(functions).ParseFS(ui.Files, patterns...)
 		if err != nil {
 			return nil, err
 		}
 		// Add the 'layout' template to the template set.
-		ts, err = ts.ParseGlob("./ui/html/partials/*.tmpl")
-		if err != nil {
-			return nil, err
-		}
-		// Parse the 'page' template itself.
-		ts, err = ts.ParseFiles(page)
-		if err != nil {
-			return nil, err
-		}
+		//ts, err = ts.ParseGlob("./ui/html/partials/*.tmpl")
+		//if err != nil {
+		//	return nil, err
+		//}
+		//// Parse the 'page' template itself.
+		//ts, err = ts.ParseFiles(page)
+		//if err != nil {
+		//	return nil, err
+		//}
 
 		cache[name] = ts
 	}
